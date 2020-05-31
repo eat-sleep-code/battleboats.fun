2032b0a8a03efa61be803faa567164c8a4ded597
eat-sleep-code <info@eat-sleep-code.com>
2020-05-30 18:20:51 -0700

    autocommit

--- src/js/game.js
+++ src/js/game.js
@@ -120,10 +120,10 @@ $(document).ready(function () {
 		for (y = 0; y < gridY; ++y) {
 			for (x = 0; x < gridX; ++x) {
 				if (isEnemy) {
-					innerHtml += '<a href="#" class="grid enemy" data-y="' + y + '" data-x="' + x + '"><img name="pc' + y + '_' + x + '" src="' + prefix + '100' + extension + '"></a>';
+					innerHtml += '<img name="pc' + y + '_' + x + '" src="' + prefix + '100' + extension + '" class="grid enemy" data-y="' + y + '" data-x="' + x + '">';
 				}
 				else {
-					innerHtml += '<a href="#" class="grid allied" data-y="' + y + '" data-x="' + x + '"><img name="ply' + y + '_' + x + '" src="' + prefix + allied[y][x][0] + extension + '"></a>';
+					innerHtml += '<img name="ply' + y + '_' + x + '" src="' + prefix + allied[y][x][0] + extension + '" class="grid allied" data-y="' + y + '" data-x="' + x + '">';
 				}
 			}
 		}
